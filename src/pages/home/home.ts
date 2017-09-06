06a4ac4ee975c78861314f923058e726e04cfa33
Joao Abranches <joao@INTELECTO.com>
2017-09-06 08:07:00 -0400

    Added searchbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef, Renderer } from '@angular/core';
+import { NavController, Searchbar } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('searchbar', { read: ElementRef }) searchbarRef: ElementRef;
+  @ViewChild('searchbar') searchbarElement: Searchbar;
+  search: boolean    = false;
+  queryText: string;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  toggleSearch() {
+    if (this.search) {
+      this.search = false;
+    } else {
+      this.search = true;
+      this.searchbarElement.setFocus();
+    }
+  }
+
+  searchAction(texto: any) {
+    let val = texto.target.value;
+    //implement search
   }
 
 }
